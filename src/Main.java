6678a9443679421b25f2bfb427747cf4eaf9ed75
Daria Slizkaya <Slizkaya.daria21@gmail.com>
2021-08-11 13:20:00 +0300

    resolve

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int depositAmount = 2000;
+        int bonus;
+        if (depositAmount>1000) {
+            bonus = depositAmount / 100;
+        } else {
+            bonus = 0;
+        }
+        int totalAmount = amount + depositAmount + bonus;
+        System.out.println(totalAmount);
+    }
 
 
 }
